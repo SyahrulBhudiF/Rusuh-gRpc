17ef69da9785e01796929c2fa7caa5905a01374a
SyahrulBhudiF <syahrul4w@gmail.com>
2025-05-27 21:49:18 +0700

    chore: adding prost-validate

--- build.rs
+++ build.rs
@@ -1,8 +1,13 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-changed=proto/auth.proto");
+
+    let descriptor_path =
+        std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap()).join("file_descriptor_set.bin");
+
     tonic_build::configure()
-        .file_descriptor_set_path("descriptor.bin")
+        .file_descriptor_set_path(descriptor_path)
         .out_dir("src/pb")
         .compile_protos(&["proto/auth.proto"], &["proto"])?;
+
     Ok(())
 }
